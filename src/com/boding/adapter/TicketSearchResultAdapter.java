11de61ce0b1d5084153dbedf2aa7e109bc01e6b7
etherge <etherge@163.com>
2014-07-02 23:43:57 +0800

    delete src

--- src/com/boding/adapter/TicketSearchResultAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.boding.adapter;
-
-import android.widget.BaseExpandableListAdapter;
-import android.widget.Filter;
-
-public abstract class TicketSearchResultAdapter extends BaseExpandableListAdapter{
-	public abstract boolean isGgroupExpandable(int groupPosition);
-	public abstract Filter getFilter();
-}
--- src/com/boding/constants/SharedPreferencesAttributes.java
+++
@@ -1,9 +0,0 @@
-package com.boding.constants;
-
-public class SharedPreferencesAttributes {
-	public static final String IS_FIRSTRUN = "isFirstRun";
-	public static final String IS_AUTOLOGIN = "isAutoLogin";
-	public static final String LOGIN_EXPIREDATE = "loginExpireDate";
-	public static final String LOGIN_USERNAME = "loginUserName";
-	public static final String LOGIN_PASSWORD = "loginPassword";
-}
